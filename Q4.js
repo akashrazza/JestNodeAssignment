02b75ae93154d0a2a7e79fd0c980a122ab8ca17f
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:40:06 +0530

    Update Q4.js

--- Q4.js
+++ Q4.js
@@ -1,15 +1,19 @@
 var fs = require('fs');
 var input_data = ""
+//Create Read Stream on file input.txt
 var readstream = fs.createReadStream('input.txt');
 
+//on data event
 readstream.on('data',(data)=>{
     input_data=input_data+data;
 })
 
+//on end event
 readstream.on('end',()=>{
     console.log(input_data)
 })
 
+//on getting error event
 readstream.on('error',(err)=>{
     console.log(err);
 })
