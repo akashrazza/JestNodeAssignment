d3a113ada3792fb579d0d43dce6ea99bd7d0bfa5
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:41:34 +0530

    Update Q5.js

--- Q5.js
+++ Q5.js
@@ -1,18 +1,22 @@
 var fs = require('fs');
-var input_data = ""
+var input_data = "";
+//create Read Stream
 var readstream = fs.createReadStream('input.txt');
 
+//on getting data event
 readstream.on('data',(data)=>{
     input_data=input_data+data;
 
 })
 
+//on end event
 readstream.on('end',()=>{
     console.log("Text File Length : ",input_data.length);
     console.log("Text Word Length : ",input_data.split(" ").length);
     console.log("Text Line Length : ",input_data.split(".").length);
 })
 
+//on getting error event
 readstream.on('error',(err)=>{
     console.log(err);
 })
