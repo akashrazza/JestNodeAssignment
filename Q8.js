ccbf38aa802882871fce899b69409a20bcade997
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:46:40 +0530

    Update Q8.js

--- Q8.js
+++ Q8.js
@@ -1,11 +1,18 @@
 var fs = require('fs');
-var data = "Inputed data"
+var data = "Inputed data";
+
+//Creating Write stream
 var writestream = fs.createWriteStream('input1.txt');
+
+//Writing into stream
 writestream.write(data);
 
+//on finish event 
 writestream.on('finish',()=>{
     console.log(data);
 })
+
+//on error event
 writestream.on('error',(err)=>{
     console.log(err);
 })
