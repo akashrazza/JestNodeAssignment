611958300dc8e3c7ea9483487bd3a064c41cd03a
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:20:58 +0530

    Update Q10.js

--- Q10.js
+++ Q10.js
@@ -1,11 +1,24 @@
 var os = require('os');
 var fs= require('fs');
+
+//Create dir Node
 fs.mkdir("Node",()=>{console.log("Folder Created")});
+
+//Creating Stream
 var write_stream = fs.createWriteStream('Node/input.txt');
+
+//Writing into Stream
 write_stream.write("");
+
+//Event Finish on Writing into straeam
 write_stream.on('finish',()=>{console.log("created")})
+
+//handle Error on writing Stream
 write_stream.on('error',(err)=>{console.log(err)})
+
 console.log("File created")
+//unlinking file
 fs.unlink('Node/input.txt',(err,data)=>{
     if (err) throw err;
-    console.log("Folder Deleted")})
+    console.log("Folder Deleted")
+})
