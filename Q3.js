cbadbd6b67aa71ab70c24e528b2bd12edf4a9478
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:38:51 +0530

    Update Q3.js

--- Q3.js
+++ Q3.js
@@ -1,20 +1,27 @@
 var event = require('events');
 var eventEmitter  =new event.EventEmitter();
 
+//Listner 1
 var listner1 = function listner1(){
     console.log("This is Listner1");
 }
+//Listner 2
 var listner2 = function listner2(){
     console.log("This is Listner2");
 }
 
+//Add Event and Listners
 eventEmitter.addListener('myevent',listner1);
 eventEmitter.addListener('myevent',listner2);
 
+//Event Emit
 eventEmitter.emit('myevent');
 
+//Get Count of All Event
 console.log("The total listners in myevent : ",eventEmitter.listenerCount('myevent'));
 
+//Remove Listners
 eventEmitter.removeListener('myevent',listner1);
 
+//Get Listner Count
 console.log("The total listners after removing in myevent : ",eventEmitter.listenerCount('myevent'));
