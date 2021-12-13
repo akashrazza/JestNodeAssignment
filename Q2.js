3859e0b1ebf1f44f96164f7f80a196aea57f4cdc
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:36:00 +0530

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -1,16 +1,21 @@
 var event = require('events');
 var eventEmitter  =new event.EventEmitter();
 
+//Listner 1
 var listner1 = function listner1(){
     console.log("This is Listner1");
 }
+
+//Listner 2
 var listner2 = function listner2(){
     console.log("This is Listner2");
 }
-
+//Adding Event to Listners
 eventEmitter.on('myevent',listner1);
 eventEmitter.on('myevent',listner2);
 
+//Event emit
 eventEmitter.emit('myevent');
 
+//Get All Names of Event
 console.log(eventEmitter.eventNames());
