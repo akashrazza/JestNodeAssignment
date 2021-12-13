a3988551a23a2a17073f4dea9ab7158de4589f60
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:49:32 +0530

    Update Q9.js

--- Q9.js
+++ Q9.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
-// var readstream = fs.createReadStream('input1.txt')
+
+//creating reading file in sync
 var read_data = fs.readFileSync('input.txt')
 
+//Create a write Stream
 var write_data = fs.createWriteStream('input2.txt');
 write_data.write(read_data);
 
+//on Finish Event
 write_data.on('finish',()=>{console.log("File created and writed")})
+
+//on error event
 write_data.on('error',(err)=>{
     console.log(err);
 })
