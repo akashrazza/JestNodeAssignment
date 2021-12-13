64baf3d8128d37dfe3c946236c6cb972617ad571
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:18:39 +0530

    Update Q1.js

--- Q1.js
+++ Q1.js
@@ -1,14 +1,18 @@
 var event = require('events');
 var eventEmitter  =new event.EventEmitter();
 
+//Listner 1
 var listner1 = function listner1(){
     console.log("This is Listner1");
 }
+//Listenr 2
 var listner2 = function listner2(){
     console.log("This is Listner2");
 }
 
+//Connecting Listners to event my event
 eventEmitter.on('myevent',listner1);
 eventEmitter.on('myevent',listner2);
 
+//Emit to that Listner
 eventEmitter.emit('myevent');
