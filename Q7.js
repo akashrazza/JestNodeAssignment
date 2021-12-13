f615d9df39ca6e518828eda1f9c94f2e269ce907
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:44:36 +0530

    Update Q7.js

--- Q7.js
+++ Q7.js
@@ -1,12 +1,17 @@
 var readline = require('readline-sync');
 var fs = require("fs");
 
-var data = readline.question("Enter Text To seaech")
+//Input text to search
+var data = readline.question("Enter Text To search")
 read_data=""
+
+//Reading file
 var read_data =  read_data + fs.readFileSync('input.txt')
 
+//check if input exist in file
 if(read_data.includes(data)){
     
+    //Counting the occurance of input
     split_data = read_data.split("\n").join(",").split(" ").join(",").split(",");
     console.log(split_data)
     var count=0
