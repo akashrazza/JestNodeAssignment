2bc461c7291ff249ea50cf2d2140e56d076f40c3
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:33:27 +0530

    Update Q13.js

--- Q13.js
+++ Q13.js
@@ -1,12 +1,17 @@
 var dns  = require('dns');
 var hostname = require('os');
 var read_line = require('readline-sync');
-var input_data = read_line.question("Entre URL")
+
+//Input URL
+var input_data = read_line.question("Entre URL");
+
+//Getting Address and family
 dns.lookup(input_data,(err,address,family)=>{
     console.log("address",address)
     console.log("family",family)
 })
 
+//Getting host name
 dns.resolve4(input_data,(err,address)=>{
     if (err) throw err;
     console.log("all the ip address of ",JSON.stringify(address));
