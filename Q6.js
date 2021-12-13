6ccaced6cb1da45616a96361cd853073fd6082f5
Raja Kumar <44458987+akashrazza@users.noreply.github.com>
2021-12-13 13:42:49 +0530

    Update Q6.js

--- Q6.js
+++ Q6.js
@@ -1,9 +1,12 @@
 var readline = require('readline-sync');
 var fs = require("fs");
+//Input 
 var data = readline.question("Enter Text To seaech")
 read_data=""
+//Reading File input.txt in sync
 var read_data =  read_data + fs.readFileSync('input.txt')
 
+//check if input exist or not
 if(read_data.includes(data)){
     console.log("Found Data");
 }
